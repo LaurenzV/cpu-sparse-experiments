99387cff66aabf26840894c828d631e8a91af652
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-01-27 11:16:08 +0100

    Fix a test case

--- tests/issues.rs
+++ tests/issues.rs
@@ -66,7 +66,7 @@ fn issue_2_incorrect_filling_4() {
     path.line_to((28.000002, 9.));
     path.line_to((24.000002, 9.));
     path.line_to((20.000002, 9.));
-    path.move_to((16.000002, 9.));
+    path.line_to((16.000002, 9.));
     path.close_path();
 
     let mut ctx = get_ctx(64, 64, false);
