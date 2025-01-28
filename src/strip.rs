50ee4e0f1829fd2aacc35bb9d4afe4b6dc42c67e
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-01-28 21:33:12 +0100

    Give code some room to breath

--- src/strip.rs
+++ src/strip.rs
@@ -90,49 +90,55 @@ impl Tile {
     }
 }
 
-// This can be unused when SIMD is selected. Probably a good idea to make it
-// selectable at runtime; will be needed for AVX2.
-#[allow(unused)]
 pub fn render_strips_scalar(tiles: &[Tile], strip_buf: &mut Vec<Strip>, alpha_buf: &mut Vec<u32>) {
     strip_buf.clear();
+
     let mut strip_start = true;
     let mut cols = alpha_buf.len() as u32;
     let mut prev_tile = &tiles[0];
     let mut fp = prev_tile.footprint().0;
     let mut seg_start = 0;
     let mut delta = 0;
+
     // Note: the input should contain a sentinel tile, to avoid having
     // logic here to process the final strip.
     for i in 1..tiles.len() {
         let tile = &tiles[i];
-        tile.footprint();
+
         if prev_tile.loc() != tile.loc() {
             let start_delta = delta;
             let same_strip = prev_tile.loc().same_strip(&tile.loc());
+
             if same_strip {
                 fp |= 8;
             }
+
             let x0 = fp.trailing_zeros();
             let x1 = 32 - fp.leading_zeros();
             let mut areas = [[start_delta as f32; 4]; 4];
+
             for tile in &tiles[seg_start..i] {
                 delta += tile.delta();
+
                 let p0 = tile.p0.unpack();
                 let p1 = tile.p1.unpack();
-                let slope = (p1.x - p0.x) / (p1.y - p0.y);
+                let inv_slope = (p1.x - p0.x) / (p1.y - p0.y);
+
                 for x in x0..x1 {
                     let startx = p0.x - x as f32;
+
                     for y in 0..4 {
                         let starty = p0.y - y as f32;
                         let y0 = starty.clamp(0.0, 1.0);
                         let y1 = (p1.y - y as f32).clamp(0.0, 1.0);
                         let dy = y0 - y1;
+
                         // Note: getting rid of this predicate might help with
                         // auto-vectorization. That said, just getting rid of
                         // it causes artifacts (which may be divide by zero).
                         if dy != 0.0 {
-                            let xx0 = startx + (y0 - starty) * slope;
-                            let xx1 = startx + (y1 - starty) * slope;
+                            let xx0 = startx + (y0 - starty) * inv_slope;
+                            let xx1 = startx + (y1 - starty) * inv_slope;
                             let xmin0 = xx0.min(xx1);
                             let xmax = xx0.max(xx1);
                             let xmin = xmin0.min(1.0) - 1e-6;
@@ -140,8 +146,10 @@ pub fn render_strips_scalar(tiles: &[Tile], strip_buf: &mut Vec<Strip>, alpha_bu
                             let c = b.max(0.0);
                             let d = xmin.max(0.0);
                             let a = (b + 0.5 * (d * d - c * c) - xmin) / (xmax - xmin);
+
                             areas[x as usize][y] += a * dy;
                         }
+
                         if p0.x == 0.0 {
                             areas[x as usize][y] += (y as f32 - p0.y + 1.0).clamp(0.0, 1.0);
                         } else if p1.x == 0.0 {
@@ -153,12 +161,14 @@ pub fn render_strips_scalar(tiles: &[Tile], strip_buf: &mut Vec<Strip>, alpha_bu
 
             for x in x0..x1 {
                 let mut alphas = 0u32;
+                
                 for y in 0..4 {
                     let area = areas[x as usize][y];
                     // nonzero winding number rule
                     let area_u8 = (area.abs().min(1.0) * 255.0).round() as u32;
                     alphas += area_u8 << (y * 8);
                 }
+
                 alpha_buf.push(alphas);
             }
 
@@ -169,17 +179,21 @@ pub fn render_strips_scalar(tiles: &[Tile], strip_buf: &mut Vec<Strip>, alpha_bu
                     col: cols,
                     winding: start_delta,
                 };
+
                 strip_buf.push(strip);
             }
+
             cols += x1 - x0;
             fp = if same_strip { 1 } else { 0 };
 
             strip_start = !same_strip;
             seg_start = i;
+
             if !prev_tile.loc().same_row(&tile.loc()) {
                 delta = 0;
             }
         }
+
         fp |= tile.footprint().0;
 
         prev_tile = tile;
