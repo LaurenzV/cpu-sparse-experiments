b5e2ed016b05ea777d1fe158ba224b79ee16bfc2
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-02-21 18:51:52 +0100

    Step 1 towards strips

--- crates/sparse_primitives/src/fine/compose.rs
+++ crates/sparse_primitives/src/fine/compose.rs
@@ -1,127 +1,137 @@
 use crate::execute::Scalar;
-use crate::fine::COLOR_COMPONENTS;
+use crate::fine::{ScratchBuf, COLOR_COMPONENTS};
 
 pub(crate) trait Compose {
     fn compose_fill(target: &mut [u8], cs: &[u8; COLOR_COMPONENTS], compose: peniko::Compose);
+    fn compose_strip(
+        target: &mut [u8],
+        cs: &[u8; COLOR_COMPONENTS],
+        alphas: &[u32],
+        compose: peniko::Compose
+    );
 }
 
 pub(crate) mod scalar {
     use crate::execute::Scalar;
     use crate::fine::compose::Compose;
     use crate::fine::{COLOR_COMPONENTS, TOTAL_STRIP_HEIGHT};
-    use crate::util::scalar::{div_255, splat_x4};
 
     impl Compose for Scalar {
         fn compose_fill(target: &mut [u8], cs: &[u8; COLOR_COMPONENTS], compose: peniko::Compose) {
             match compose {
-                peniko::Compose::Copy => src_copy(target, cs),
-                peniko::Compose::SrcOver => src_over(target, cs),
-                peniko::Compose::DestOver => dest_over(target, cs),
-                peniko::Compose::SrcAtop => src_atop(target, cs),
-                peniko::Compose::DestOut => dest_out(target, cs),
-                peniko::Compose::Xor => xor(target, cs),
-                peniko::Compose::Plus => plus(target, cs),
-                peniko::Compose::Dest => dest(target, cs),
-                peniko::Compose::Clear => clear(target, cs),
+                peniko::Compose::Copy => fill::src_copy(target, cs),
+                peniko::Compose::SrcOver => fill::src_over(target, cs),
+                peniko::Compose::DestOver => fill::dest_over(target, cs),
+                peniko::Compose::SrcAtop => fill::src_atop(target, cs),
+                peniko::Compose::DestOut => fill::dest_out(target, cs),
+                peniko::Compose::Xor => fill::xor(target, cs),
+                peniko::Compose::Plus => fill::plus(target, cs),
+                peniko::Compose::Dest => fill::dest(target, cs),
+                peniko::Compose::Clear => fill::clear(target, cs),
                 _ => unimplemented!(),
             }
         }
     }
 
-    // All the formulas in the comments are with premultiplied alpha for Cs and Cb.
+    mod fill {
+        // All the formulas in the comments are with premultiplied alpha for Cs and Cb.
 
-    /// Composite using `SrcOver` (Cs + Cb * (1 – αs)).
-    pub(crate) fn src_over(target: &mut [u8], cs: &[u8; COLOR_COMPONENTS]) {
-        let _as = cs[3];
-        let cs = splat_x4(cs);
+        use crate::fine::{COLOR_COMPONENTS, TOTAL_STRIP_HEIGHT};
+        use crate::util::scalar::{div_255, splat_x4};
 
-        let inv_as = (255 - _as) as u16;
+        /// Composite using `SrcOver` (Cs + Cb * (1 – αs)).
+        pub(crate) fn src_over(target: &mut [u8], cs: &[u8; COLOR_COMPONENTS]) {
+            let _as = cs[3];
+            let cs = splat_x4(cs);
 
-        for cb in target.chunks_exact_mut(TOTAL_STRIP_HEIGHT) {
-            for i in 0..TOTAL_STRIP_HEIGHT {
-                cb[i] = cs[i] + div_255(cb[i] as u16 * inv_as) as u8;
+            let inv_as = (255 - _as) as u16;
+
+            for cb in target.chunks_exact_mut(TOTAL_STRIP_HEIGHT) {
+                for i in 0..TOTAL_STRIP_HEIGHT {
+                    cb[i] = cs[i] + div_255(cb[i] as u16 * inv_as) as u8;
+                }
             }
         }
-    }
 
-    /// Composite using `SrcCopy` (Cs).
-    pub(crate) fn src_copy(target: &mut [u8], cs: &[u8; COLOR_COMPONENTS]) {
-        let cs = splat_x4(cs);
+        /// Composite using `SrcCopy` (Cs).
+        pub(crate) fn src_copy(target: &mut [u8], cs: &[u8; COLOR_COMPONENTS]) {
+            let cs = splat_x4(cs);
 
-        for cb in target.chunks_exact_mut(TOTAL_STRIP_HEIGHT) {
-            cb.copy_from_slice(&cs);
-        }
-    }
-
-    /// Composite using `DestOver` (Cs * (1 – αb) + Cb).
-    pub(crate) fn dest_over(target: &mut [u8], cs: &[u8; COLOR_COMPONENTS]) {
-        for cb in target.chunks_exact_mut(4) {
-            let inv_ab = (255 - cb[3]) as u16;
-
-            for i in 0..COLOR_COMPONENTS {
-                cb[i] = div_255(cs[i] as u16 * inv_ab) as u8 + cb[i];
+            for cb in target.chunks_exact_mut(TOTAL_STRIP_HEIGHT) {
+                cb.copy_from_slice(&cs);
             }
         }
-    }
 
-    /// Composite using `SrcAtop` (Cs * αb + Cb * (1 – αs)).
-    pub(crate) fn src_atop(target: &mut [u8], cs: &[u8; COLOR_COMPONENTS]) {
-        for cb in target.chunks_exact_mut(4) {
-            let inv_as = (255 - cs[3]) as u16;
+        /// Composite using `DestOver` (Cs * (1 – αb) + Cb).
+        pub(crate) fn dest_over(target: &mut [u8], cs: &[u8; COLOR_COMPONENTS]) {
+            for cb in target.chunks_exact_mut(4) {
+                let inv_ab = (255 - cb[3]) as u16;
 
-            for i in 0..COLOR_COMPONENTS {
-                let ab = cb[3] as u16;
-                let im1 = div_255(cs[i] as u16 * ab) as u8;
-                let im2 = div_255(cb[i] as u16 * inv_as) as u8;
-
-                cb[i] = im1 + im2;
+                for i in 0..COLOR_COMPONENTS {
+                    cb[i] = div_255(cs[i] as u16 * inv_ab) as u8 + cb[i];
+                }
             }
         }
-    }
 
-    /// Composite using `DestOut` (Cb * (1 - as)).
-    pub(crate) fn dest_out(target: &mut [u8], cs: &[u8; COLOR_COMPONENTS]) {
-        let inv_as = 255 - cs[3] as u16;
+        /// Composite using `SrcAtop` (Cs * αb + Cb * (1 – αs)).
+        pub(crate) fn src_atop(target: &mut [u8], cs: &[u8; COLOR_COMPONENTS]) {
+            for cb in target.chunks_exact_mut(4) {
+                let inv_as = (255 - cs[3]) as u16;
 
-        for cb in target.chunks_exact_mut(4) {
-            for i in 0..COLOR_COMPONENTS {
-                cb[i] = div_255(cb[i] as u16 * inv_as) as u8;
+                for i in 0..COLOR_COMPONENTS {
+                    let ab = cb[3] as u16;
+                    let im1 = div_255(cs[i] as u16 * ab) as u8;
+                    let im2 = div_255(cb[i] as u16 * inv_as) as u8;
+
+                    cb[i] = im1 + im2;
+                }
             }
         }
-    }
 
-    /// Composite using `Xor` (Cs * (1 - αb) + Cb * (1 - αs)).
-    pub(crate) fn xor(target: &mut [u8], cs: &[u8; COLOR_COMPONENTS]) {
-        let inv_as = 255 - cs[3] as u16;
+        /// Composite using `DestOut` (Cb * (1 - as)).
+        pub(crate) fn dest_out(target: &mut [u8], cs: &[u8; COLOR_COMPONENTS]) {
+            let inv_as = 255 - cs[3] as u16;
 
-        for cb in target.chunks_exact_mut(4) {
-            for i in 0..COLOR_COMPONENTS {
-                let inv_ab = 255 - cb[3] as u16;
-                let im1 = div_255(cs[i] as u16 * inv_ab) as u8;
-                let im2 = div_255(cb[i] as u16 * inv_as) as u8;
-
-                cb[i] = im1 + im2;
+            for cb in target.chunks_exact_mut(4) {
+                for i in 0..COLOR_COMPONENTS {
+                    cb[i] = div_255(cb[i] as u16 * inv_as) as u8;
+                }
             }
         }
-    }
 
-    /// Composite using `Plus` (Cs + Cb).
-    pub(crate) fn plus(target: &mut [u8], cs: &[u8; COLOR_COMPONENTS]) {
-        let cs = splat_x4(cs);
+        /// Composite using `Xor` (Cs * (1 - αb) + Cb * (1 - αs)).
+        pub(crate) fn xor(target: &mut [u8], cs: &[u8; COLOR_COMPONENTS]) {
+            let inv_as = 255 - cs[3] as u16;
 
-        for cb in target.chunks_exact_mut(TOTAL_STRIP_HEIGHT) {
-            for i in 0..TOTAL_STRIP_HEIGHT {
-                cb[i] = cs[i].saturating_add(cb[i]);
+            for cb in target.chunks_exact_mut(4) {
+                for i in 0..COLOR_COMPONENTS {
+                    let inv_ab = 255 - cb[3] as u16;
+                    let im1 = div_255(cs[i] as u16 * inv_ab) as u8;
+                    let im2 = div_255(cb[i] as u16 * inv_as) as u8;
+
+                    cb[i] = im1 + im2;
+                }
             }
         }
-    }
 
-    /// Composite using `Dest` (Cb).
-    pub(crate) fn dest(_: &mut [u8], _: &[u8; COLOR_COMPONENTS]) {}
+        /// Composite using `Plus` (Cs + Cb).
+        pub(crate) fn plus(target: &mut [u8], cs: &[u8; COLOR_COMPONENTS]) {
+            let cs = splat_x4(cs);
 
-    /// Composite using `Clear` (0).
-    pub(crate) fn clear(target: &mut [u8], _: &[u8; COLOR_COMPONENTS]) {
-        target.fill(0);
+            for cb in target.chunks_exact_mut(TOTAL_STRIP_HEIGHT) {
+                for i in 0..TOTAL_STRIP_HEIGHT {
+                    cb[i] = cs[i].saturating_add(cb[i]);
+                }
+            }
+        }
+
+        /// Composite using `Dest` (Cb).
+        pub(crate) fn dest(_: &mut [u8], _: &[u8; COLOR_COMPONENTS]) {}
+
+        /// Composite using `Clear` (0).
+        pub(crate) fn clear(target: &mut [u8], _: &[u8; COLOR_COMPONENTS]) {
+            target.fill(0);
+        }
     }
 }
 
