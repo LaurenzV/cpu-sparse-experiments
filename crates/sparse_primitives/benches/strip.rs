7a1155a64764cfa5b3c763a104bf3bfb581c1a3a
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-02-23 17:30:13 +0100

    Deduplicate strip benchmark harness

--- crates/sparse_primitives/benches/strip.rs
+++ crates/sparse_primitives/benches/strip.rs
@@ -16,8 +16,8 @@ const SEED: [u8; 32] = [0; 32];
 pub fn strip(c: &mut Criterion) {
     let mut g = c.benchmark_group("strip");
 
-    macro_rules! strip {
-        ($name:ident, $compose:path) => {
+    macro_rules! strip_single {
+        ($name:ident, $compose:path, $exec:ident) => {
             let mut alphas = vec![];
             let mut rng = StdRng::from_seed(SEED);
 
@@ -25,30 +25,12 @@ pub fn strip(c: &mut Criterion) {
                 alphas.push(rng.next_u32());
             }
 
-            g.bench_function(format!("{} - scalar", stringify!($name)), |b| {
-                b.iter(|| {
-                    let mut out = vec![];
-                    let mut fine = Fine::<Scalar>::new(WIDE_TILE_WIDTH, STRIP_HEIGHT, &mut out);
-                    let mut color = ColorIter::new(false);
-
-                    for _ in 0..STRIP_ITERS {
-                        fine.strip(
-                            0,
-                            WIDE_TILE_WIDTH,
-                            &alphas,
-                            &color.next().unwrap().into(),
-                            $compose,
-                        );
-                    }
-                })
-            });
-
-            #[cfg(all(target_arch = "aarch64", feature = "simd"))]
-            {
-                g.bench_function(format!("{} - neon", stringify!($name)), |b| {
+            g.bench_function(
+                format!("{} - {}", stringify!($name), stringify!($exec)),
+                |b| {
                     b.iter(|| {
                         let mut out = vec![];
-                        let mut fine = Fine::<Neon>::new(WIDE_TILE_WIDTH, STRIP_HEIGHT, &mut out);
+                        let mut fine = Fine::<$exec>::new(WIDE_TILE_WIDTH, STRIP_HEIGHT, &mut out);
                         let mut color = ColorIter::new(false);
 
                         for _ in 0..STRIP_ITERS {
@@ -61,8 +43,16 @@ pub fn strip(c: &mut Criterion) {
                             );
                         }
                     })
-                });
-            }
+                },
+            );
+        };
+    }
+
+    macro_rules! strip {
+        ($name:ident, $compose:path) => {
+            strip_single!($name, $compose, Scalar);
+            #[cfg(all(target_arch = "aarch64", feature = "simd"))]
+            strip_single!($name, $compose, Neon);
         };
     }
 
